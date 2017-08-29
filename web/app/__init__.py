e8059e9ca88c3e289bce29bac881f54846850ffd
John DeSilvio <john.desilvio@yahoo.com>
2017-08-28 22:19:48 -0400

    Configure Flask-WTF

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CSRFProtect
+from forms import MyForm
 
 
 app = Flask(__name__)
 Bootstrap(app)
+CSRFProtect(app)
+app.config.from_object('dev_config')
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
